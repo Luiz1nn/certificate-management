c933a33073b577d864b967bb3d957bfc9ea90dd4
Luiz1nn <luisfernandobr2003@gmail.com>
2025-05-31 21:26:46 -0300

    ✨ feat: add routing with certificates page as default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from "@angular/router"
+import { CertificatesPage } from "./pages/certificates/certificates.page"
 
-export const routes: Routes = []
+export const routes: Routes = [
+	{
+		path: "",
+		component: CertificatesPage,
+	},
+]
---
+++ src/app/pages/certificates/certificates.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates',
+  imports: [],
+  templateUrl: './certificates.page.html',
+  styleUrl: './certificates.page.css'
+})
+export class CertificatesPage {
+
+}
