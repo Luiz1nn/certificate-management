42ab217b787f1a442d0a3674566b17ae62ed766c
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-13 13:18:54 -0300

    ✨ feat: add routerLink to 'generate-certificate' button in NoCertificates component

--- src/app/pages/certificates/_components/no-certificates/no-certificates.component.ts
+++ src/app/pages/certificates/_components/no-certificates/no-certificates.component.ts
@@ -1,9 +1,10 @@
 import { ButtonComponent } from "@/components/button/button.component"
 import { Component } from "@angular/core"
+import { RouterLink } from "@angular/router"
 
 @Component({
 	selector: "app-no-certificates",
-	imports: [ButtonComponent],
+	imports: [ButtonComponent, RouterLink],
 	standalone: true,
 	templateUrl: "./no-certificates.component.html",
 	styleUrl: "./no-certificates.component.css",
